2cb2dce2d2f7b296c05eef7ef092b2de877a97f3
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2020-10-15 10:19:24 -0700

    Create Distance.cpp

---
+++ Distance.cpp
@@ -0,0 +1,20 @@
+
+
+
+
+// Distance::operator+( Distance D, Distance d2 ) { 
+//             float addInches = d2.inches + D.inches;
+//             int feetAdd = 0;
+//             float inchAdd = 0;
+//             if ( addInches > 12 )
+//             {
+//                 feetAdd = addInches / 12;
+//                 inchAdd = addInches - ( feetAdd * 12 );
+//             }
+//             else 
+//             {
+//                 inchAdd = addInches;
+//             }
+//             Distance newDist = Distance( d2.feet + D.feet + feetAdd, inchAdd);
+//             return newDist;            
+//         }
